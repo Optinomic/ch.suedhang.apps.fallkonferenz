cf1ef44ea93414f8bab7b51ee96a7734db1758d8
Thomas Feron <thomas.feron@redspline.com>
2018-04-21 17:21:43 +0200

    First commit (moved from Optinomic/apps)

---
+++ _data_toc.js
@@ -0,0 +1,42 @@
+
+d.data_app = {
+    "actinfo_ein": "ch.suedhang.apps.actinfo_ein",
+    "actinfo_aus": "ch.suedhang.apps.actinfo_aus",
+    "tmt": "ch.suedhang.apps.tmt_V3",
+    "asrs": "ch.suedhang.apps.asrs",
+    "aase": "ch.suedhang.apps.aase-g",
+    "bscl": "ch.suedhang.apps.bscl_anq",
+    "bdi": "ch.suedhang.apps.bdi",
+    "isk": "ch.suedhang.apps.isk",
+    "sci": "ch.suedhang.apps.sci",
+    "whoqol": "com.optinomic.apps.whoqol"
+};
+
+d.toc = [{
+    "name": "Klientenmonitoringsystem (act-Info)",
+    "anchor": "content_actinfo"
+}, {
+    "name": "Trail Making Test (TMT)",
+    "anchor": "content_tmt"
+}, {
+    "name": "ADHS-Screening (ASRS)",
+    "anchor": "content_asrs"
+}, {
+    "name": "Alcohol Abstinence Self-Efficacy (AASE)",
+    "anchor": "content_aase"
+}, {
+    "name": "Brief Symptom Checklist (BSCL)",
+    "anchor": "content_bscl"
+}, {
+    "name": "Beck Depressions-Inventar (BDI-II)",
+    "anchor": "content_bdi"
+}, {
+    "name": "Inventar Sozialer Kompetenzen (ISK)",
+    "anchor": "content_isk"
+}, {
+    "name": "Stress-Coping-Inventar (SCI)",
+    "anchor": "content_sci"
+}, {
+    "name": "WHO Quality of Life (WHOQOL-BREF)",
+    "anchor": "content_whoqol"
+}];
